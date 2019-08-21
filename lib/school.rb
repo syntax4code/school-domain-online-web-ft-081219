77003aea6a74ab323e1b21d743237156cadb9668
Warren Loy Harper II <warrenharper2@att.net>
2019-08-21 17:04:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,16 @@
 class School
-  def initialize(name)
+  def initialize(name) #Person class' #name method ed to as a "getter" or reader method. It returns information stored in an instance variable. 
     @name = name
     @roster = {}
   end
 
-  def roster
+  def roster   #this is the setter method
     @roster
   end
 
   def add_student(name, grade)
     if @roster[grade] != nil
-      @roster[grade] << name
+      @roster[grade] << name     #push name into the corresponding grade
     else
       @roster[grade] = [name]
     end
@@ -24,5 +24,23 @@ class School
     @roster.each do |key, value|
       value.sort!
     end
+
+
+  def add_student(name, grade)
+    if @roster[grade] 
+     
+    else
+      @roster[grade] = []
+       @roster[grade] << name
+    end
   end
+  def grade(grade)
+    @roster[grade]
+end
+def sort 
+  @roster.each do |name, grade|
+    grade.sort! #using the bang causes the data collected to be destroyed
+  end 
 end 
+end 
+
